3de6ac51b7ada0369ffbf2f22413db1807a84acd
Vini Dalvino <37189791+PissMan420@users.noreply.github.com>
2022-03-04 20:26:06 -0500

    Removed key listening Removed advanced key listening feature.

--- main.py
+++ main.py
@@ -20,20 +20,5 @@ while cam.isOpened():
     key = cv2.waitKey(10)
     if key == ord('q'):
         cam.release()
-    elif key in [ord('c'), ord('z')]:
-        if not color_conversion:
-            color_conversion = 0
-        if key == ord('c'):
-            if color_conversion + 1 <= 143:
-                if color_conversion + 1 == 8:
-                    color_conversion += 2
-                else:
-                    color_conversion += 1
-        elif key == ord('z'):
-            if color_conversion - 1 > 0:
-                color_conversion -= 1
-        print('changing color conversion mode to %d' % color_conversion)
-    if color_conversion:
-        mat = cv2.cvtColor(mat, color_conversion)
     facture = recognizer.reconnaitre_facture(mat)
     cv2.imshow('recipe', facture)
