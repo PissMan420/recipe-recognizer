31f3f26f293c2df3d6edd42a58ba9b994e4b7ba2
Vini Dalvino <37189791+PissMan420@users.noreply.github.com>
2022-03-07 13:34:10 -0500

    Implemented initiation code The initiation code consist of initiating GLFW, Open GL and ImGui

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#/bin/python3
+import sys
+
 import cv2
+import glfw
+import imgui
+import OpenGL.GL as gl
+
+from imgui.integrations.glfw import GlfwRenderer
 
 import recognizer
 
@@ -13,12 +21,28 @@ cam = cv2.VideoCapture()
 cam.open(0)
 mat = None
 color_conversion = None
+imgui.create_context()
+glfw.init()
+WIN_TITLE = 'recipe recognition'
+if sys.platform == 'darwin':
+    # Mac OS Specific hints
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    glfw.window_hint(glfw.COCOA_RETINA_FRAMEBUFFER, True)
+    glfw.window_hint(glfw.COCOA_GRAPHICS_SWITCHING, gl.GL_TRUE)
+    glfw.window_hint_string(glfw.COCOA_FRAME_NAME, WIN_TITLE)
+elif sys.plarform in ['linux', 'linux2']:
+    # Linux Specific hints
+    glfw.window_hint_string(glfw.X11_CLASS_NAME, 'cls' + WIN_TITLE)
+    glfw.window_hint_string(glfw.X11_INSTANCE_NAME, WIN_TITLE)
+glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+window = glfw.create_window(1280, 720, WIN_TITLE, None, None)
+glfw.make_context_current(window)
+if not window: exit(1)
 
 while cam.isOpened():
     _succ, mat = cam.read()
 
-    key = cv2.waitKey(10)
-    if key == ord('q'):
-        cam.release()
     facture = recognizer.reconnaitre_facture(mat)
     cv2.imshow('recipe', facture)
