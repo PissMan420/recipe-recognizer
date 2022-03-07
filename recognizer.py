d58c86bd916ed04cacaa5e0c6e411905b7515ac9
Vini Dalvino <37189791+PissMan420@users.noreply.github.com>
2022-03-07 11:58:49 -0500

    Implemented an keyreading system

--- recognizer.py
+++ recognizer.py
@@ -1,6 +1,7 @@
 from typing_extensions import TypedDict
 from typing import Optional
 
+from cv2 import waitKey
 import cv2
 
 
@@ -10,10 +11,23 @@ class Facture(TypedDict):
     code_concour: Optional[str]
 
 
+val = 0
 def reconnaitre_facture(mat):
     """
     Cette fonction localise une facture dans une image. Cette fonction
     retourne la facture découverte.
     """
-    cv2.cvtColor(mat, cv2.COLOR_BGR2RGB, mat)
+    keys = [(ord('z'), (lambda x: x(0.01))), (ord('x'), (lambda x: x(-0.01))),
+            (ord('q'), lambda _: exit(1337))]
+    for code, func in keys:
+        if code == waitKey(10):
+            result = func(lambda: val)
+            # If it's a slider func
+            if str(type(result)) == "<class 'function'>":
+                newv = val + result()
+                print('old val: %d, new val: %d', val, newv)
+                val = newv
+                break
+    mat = cv2.cvtColor(mat, cv2.COLOR_BGR2RGB)
+    cv2.denoise_TVL1(mat, None, 1, niters=3)
     return mat
